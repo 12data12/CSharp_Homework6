747951e0a89a4217ede3822ce2b01cc056f249ec
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-09 15:51:06 +0300

    Final commit

---
+++ Homework6_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("How many numbers are you planning to enter? ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[m];
+
+void InputNumbers(int m)
+{
+    for(int i = 0; i < m; i++)
+    {
+        Console.Write($"Enter {i+1} number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int Comparison(int [] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >0) count +=1;
+    }
+    return count;
+}
+InputNumbers(m);
+Console.WriteLine($"The number of numbers greater than zero is {Comparison(array)}");
---
+++ Homework6_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Enter b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+if (k1==k2 || k1==k2 && b1==b2)
+{
+    Console.Write("Two lines are pararrel or coincident");
+}
+else
+{
+Console.Write($"Two lines intersect at the point with the coordinates ({x};{y})");
+}
